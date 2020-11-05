1f4902abffbe75fabce04f612864bb879294f89b
Roy Golan <rgolan@redhat.com>
2020-11-05 15:16:15 +0200

    fix failing identity ping
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- pkg/service/driver.go
+++ pkg/service/driver.go
@@ -25,7 +25,9 @@ type kubevirtCSIDriver struct {
 // NewkubevirtCSIDriver creates a driver instance
 func NewkubevirtCSIDriver(infraClusterClient kubernetes.Clientset, virtClient kubevirt.Client, nodeId string) *kubevirtCSIDriver {
 	d := kubevirtCSIDriver{
-		IdentityService:    &IdentityService{},
+		IdentityService:    &IdentityService{
+			infraClusterClient: virtClient,
+		},
 		ControllerService:  &ControllerService{infraClusterClient: infraClusterClient},
 		NodeService:        &NodeService{nodeId: nodeId, kubevirtClient: virtClient},
 	}
